14c676d9a3bbaf2fd7f5c15313bc9c1757b7d5c9
leeg358 <leeg358@newschool.edu>
2021-02-05 03:16:42 -0800

    update

---
+++ project-02/js/global.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+    let mouseCursor = document.querySelector(".cursor")
+
+    window.addEventListener("mousemove", cursor)
+
+    function cursor(e){
+        mouseCursor.style.top = e.pageY + "px"
+        mouseCursor.style.left = e.pageX + "px"
+    }
+});
