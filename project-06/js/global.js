9f188c88134e55ab514ad3e6a75b27058257dc11
leeg358 <leeg358@newschool.edu>
2021-03-10 13:02:04 -0800

    facts

---
+++ project-06/js/global.js
@@ -0,0 +1,48 @@
+function One() {
+    alert("Dinosaurs are a group of reptiles that dominated the land for over 140 million years (more than 160 million years in some parts of the world). They evolved diverse shapes and sizes, from the fearsome giant Spinosaurus to the chicken-sized Microraptor, and were able to survive in a variety of ecosystems.");
+  }
+
+  function Two() {
+    alert("The first dinosaur to be named was Megalosaurus. It was named in 1824 by Reverend William Buckland. Megalosaurus means ‘great lizard’ and it was about 9 metres long, and 3 metres tall!");
+  }
+
+  function Three() {
+    alert("There are over 700 different species of dinosaurs that have currently been identified and named!");
+  }
+
+  function Four() {
+    alert("Dinosaurs generally are named after a characteristic body feature, after the place where they were found, or after a person involved in the discovery.");
+  }
+
+  function Five() {
+    alert("It is believed that dinosaur extinction was part of a mass extinction brought about by two massive destructive events. The first of these was the collision with the Earth of a meteorite landing in what is now the Yucatan Peninsula, of Mexico. This was followed by an enormous volcanic eruption which split what is now India in half.");
+  }
+
+  function Six() {
+    alert("Dinosaurs lived on all of the continents. At the beginning of the age of dinosaurs (during the Triassic Period, about 230 million years ago), the continents were arranged together as a single supercontinent called Pangea. During the 165 million years of dinosaur existence this supercontinent slowly broke apart.");
+  }
+
+  function Seven() {
+    alert("One of the smallest dinosaurs yet discovered are a crow-sized theropod, Microraptor, that was found in China. It is about 16 inches long, and was believed to be an adult!");
+  }
+
+  function Eight() {
+    alert("The Argentinosaurus was the largest dinosaur, and is the biggest animal ever known. They weighed 110,000 to 220,000 pounds!");
+  }
+
+  function Nine() {
+    alert("No dinosaurs were adapted to an entirely aquatic life, although some may have gone into the water to get prey. Baryonyx, for example, was found with fossilized fish scales in its stomach.\n\nThere were many aquatic reptiles that lived during the time of the dinosaurs, including plesiosaurs, nothosaurs, and mosasaurs.");
+  }
+
+  function Ten() {
+    alert("They were the pterosaurs which included Plesiosaurus, Pteranodon, Pterodactylus, Dimorphodon, Rhamphorhynchus, Quetzalcoatlus, and many others. Pterosaurs were flying, prehistoric reptiles. They were not dinosaurs, but were closely related to them.");
+  }
+
+  function Eleven() {
+    alert("Some dinosaurs ate lizards, turtles, eggs, or early mammals. Some hunted other dinosaurs or scavenged dead animals. Most, however, ate plants (but not grass, which hadn't evolved yet!)");
+  }
+
+  function Twelve() {
+    alert("https://www.thedinosaurmuseum.com/dino-facts\n\nhttps://www.nhm.ac.uk/discover/what-are-dinosaurs.html\n\nhttps://www.enchantedlearning.com/subjects/dinosaurs/questions/faq/\n\nhttps://www.usgs.gov/faqs/");
+  }
+
