ca9d4f7073d2853e675d43e325ea3ee163a7aac9
leeg358 <leeg358@newschool.edu>
2021-03-26 00:21:47 -0400

    Update global.js

--- project-08/js/global.js
+++ project-08/js/global.js
@@ -12,7 +12,7 @@ const recipe = {
       "3 tbsp olive oil ",
       "3/4 tsp salt",
       "1/2 tsp ground black pepper",
-      "2 tbsp balsamic glaze",
+      "2 tbsp balsamic glazes",
     ],
   
     name: "Roasted Brussels Sprouts with Balsamic",    
